e890caaa95f4bc13259eab72bc63c6b4538086a8
Guy-Yeh <yeknom5325@gmail.com>
2021-05-21 11:06:00 +0800

    456

--- Shopping/Global.asax.cs
+++ Shopping/Global.asax.cs
@@ -11,7 +11,7 @@ namespace Shopping
 {
     public class Global : HttpApplication
     {
-        //123
+        //123456
         void Application_Start(object sender, EventArgs e)
         {
             // 應用程式啟動時執行的程式碼
