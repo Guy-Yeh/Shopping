a40c1a2089a593d8e5d308aa908c3526b294a088
cecil1990 <84056985+cecil1990@users.noreply.github.com>
2021-06-21 18:14:07 +0800

    動態生成

---
+++ Shopping/Models/indexModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Shopping.Models
+{
+    public class indexModel
+    {
+        public string picture { get; set; }
+        public string productName { get; set; }
+        public int price { get; set; }
+        public string category { get; set; }
+
+    }
+
+}
---
+++ Shopping/Service/indexpro.cs
@@ -0,0 +1,20 @@
+﻿using Shopping.Dao;
+using Shopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shopping.Service
+{
+    public class indexpro : BaseService
+    {
+        public List<indexModel> indexproduct()
+        {
+            indexDao indexDao = new indexDao();
+            List<indexModel> index = indexDao.indexproduct();
+            return index;
+        }
+
+    }
+}
